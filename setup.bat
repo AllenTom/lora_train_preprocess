e7f712200caf1d5e2872b0ed98c8f35d5abc8fc3
AllenTom <qweasdzxczse2@gmail.com>
2023-06-11 21:06:14 +0800

    add install script

---
+++ setup.bat
@@ -0,0 +1,12 @@
+@echo off
+IF NOT EXIST venv (
+    python -m venv venv
+) ELSE (
+    echo venv folder already exists, skipping creation...
+)
+call .\venv\Scripts\activate.bat
+
+pip install --use-pep517 --upgrade -r requirements.txt
+
+echo "安装成功"
+```
